522b6e95cdc39c1269b01e7c8302f617616ce1d8
marianobrc <marianomartinez84@gmail.com>
2022-06-10 11:18:53 -0300

    Add some mock API endpoints

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
