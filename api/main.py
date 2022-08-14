76da424cfbd953f9af8a6e285a2a81a4ed30c44a
marianobrc <marianomartinez84@gmail.com>
2022-08-13 21:20:14 -0300

    Add a basic items api using class-based views

--- api/main.py
+++ api/main.py
@@ -1,18 +1,111 @@
-from fastapi import FastAPI
+from typing import NewType, Optional, List
+from uuid import UUID
 
+import sqlalchemy as sa
+from fastapi import Depends, FastAPI, Header, HTTPException
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from starlette.status import HTTP_403_FORBIDDEN, HTTP_404_NOT_FOUND
+
+from fastapi_restful.api_model import APIMessage, APIModel
+from fastapi_restful.cbv import cbv
+from fastapi_restful.guid_type import GUID
+from fastapi_restful.inferring_router import InferringRouter
+
+# Begin Setup
+UserID = NewType("UserID", UUID)
+ItemID = NewType("ItemID", UUID)
+
+Base = declarative_base()
+
+
+class ItemORM(Base):
+    __tablename__ = "item"
+
+    item_id = sa.Column(GUID, primary_key=True)
+    owner = sa.Column(GUID, nullable=False)
+    name = sa.Column(sa.String, nullable=False)
+
+
+class ItemCreate(APIModel):
+    name: str
+    owner: UserID
+
+
+class ItemInDB(ItemCreate):
+    item_id: ItemID
+
+
+def get_jwt_user(authorization: str = Header(...)) -> UserID:
+    """ Pretend this function gets a UserID from a JWT in the auth header """
+
+
+def get_db() -> Session:
+    """ Pretend this function returns a SQLAlchemy ORM session"""
+
+
+def get_owned_item(session: Session, owner: UserID, item_id: ItemID) -> ItemORM:
+    item: Optional[ItemORM] = session.query(ItemORM).get(item_id)
+    if item is not None and item.owner != owner:
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN)
+    if item is None:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+    return item
+
+
+def get_items(session: Session) -> List[ItemORM]:
+    items: List[ItemORM] = session.query(ItemORM).all()
+    return items
+
+
+# End Setup
 app = FastAPI()
+router = InferringRouter()  # Step 1: Create a router
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"status": "ok"}
 
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+@cbv(router)  # Step 2: Create and decorate a class to hold the endpoints
+class ItemCBV:
+    # Step 3: Add dependencies as class attributes
+    session: Session = Depends(get_db)
+    user_id: UserID = Depends(get_jwt_user)
+
+    @router.get("/items")
+    def list_items(self) -> List[ItemInDB]:
+        return get_items(self.session)
+
+    @router.post("/items")
+    def create_item(self, item: ItemCreate) -> ItemInDB:
+        # Step 4: Use `self.<dependency_name>` to access shared dependencies
+        item_orm = ItemORM(name=item.name, owner=self.user_id)
+        self.session.add(item_orm)
+        self.session.commit()
+        return ItemInDB.from_orm(item_orm)
+
+    @router.get("/items/{item_id}")
+    def read_item(self, item_id: ItemID) -> ItemInDB:
+        item_orm = get_owned_item(self.session, self.user_id, item_id)
+        return ItemInDB.from_orm(item_orm)
+
+    @router.put("/items/{item_id}")
+    def update_item(self, item_id: ItemID, item: ItemCreate) -> ItemInDB:
+        item_orm = get_owned_item(self.session, self.user_id, item_id)
+        item_orm.name = item.name
+        self.session.add(item_orm)
+        self.session.commit()
+        return ItemInDB.from_orm(item_orm)
+
+    @router.delete("/items/{item_id}")
+    def delete_item(self, item_id: ItemID) -> APIMessage:
+        item = get_owned_item(self.session, self.user_id, item_id)
+        self.session.delete(item)
+        self.session.commit()
+        return APIMessage(detail=f"Deleted item {item_id}")
 
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+app.include_router(router)
+
